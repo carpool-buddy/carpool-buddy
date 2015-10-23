c90fca6d77ea20230655ee541789437af14c3a8d
James Norton <james.norton@me.com>
2015-10-22 18:24:26 -0700

    changes for Erik

--- app/display_route/display_route.js
+++ app/display_route/display_route.js
@@ -11,8 +11,8 @@ function initMap() {
 
 function calculateAndDisplayRoute(directionsService, directionsDisplay) {
   directionsService.route({
-    origin: "Seattle, WA", //example
-    destination: "Bellevue, WA", //example
+    origin: document.getElementById('tripOrigin').text(), //example
+    destination: document.getElementById('tripDest').text(), //example
     travelMode: google.maps.TravelMode.DRIVING
   }, function(response, status) {
     if (status === google.maps.DirectionsStatus.OK) {
